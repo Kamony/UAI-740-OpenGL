6543f4ee293e07c4fd1ad464a2d9b41773012fba
Jiri Hauser <jirihauser94@gmail.com>
2019-01-03 00:00:38 +0100

    3d model of ship

--- src/game/MainGameLoop.java
+++ src/game/MainGameLoop.java
@@ -8,6 +8,7 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -20,83 +21,84 @@ public class MainGameLoop {
         Loader loader = new Loader();
         StaticShader shader = new StaticShader();
         Renderer renderer = new Renderer(shader);
-        float[] vertices = {
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
+// cube
+// float[] vertices = {
+//                -0.5f,0.5f,-0.5f,
+//                -0.5f,-0.5f,-0.5f,
+//                0.5f,-0.5f,-0.5f,
+//                0.5f,0.5f,-0.5f,
+//
+//                -0.5f,0.5f,0.5f,
+//                -0.5f,-0.5f,0.5f,
+//                0.5f,-0.5f,0.5f,
+//                0.5f,0.5f,0.5f,
+//
+//                0.5f,0.5f,-0.5f,
+//                0.5f,-0.5f,-0.5f,
+//                0.5f,-0.5f,0.5f,
+//                0.5f,0.5f,0.5f,
+//
+//                -0.5f,0.5f,-0.5f,
+//                -0.5f,-0.5f,-0.5f,
+//                -0.5f,-0.5f,0.5f,
+//                -0.5f,0.5f,0.5f,
+//
+//                -0.5f,0.5f,0.5f,
+//                -0.5f,0.5f,-0.5f,
+//                0.5f,0.5f,-0.5f,
+//                0.5f,0.5f,0.5f,
+//
+//                -0.5f,-0.5f,0.5f,
+//                -0.5f,-0.5f,-0.5f,
+//                0.5f,-0.5f,-0.5f,
+//                0.5f,-0.5f,0.5f
+//        };
+//
+//        float[] textureCoords = {
+//                0,0,
+//                0,1,
+//                1,1,
+//                1,0,
+//                0,0,
+//                0,1,
+//                1,1,
+//                1,0,
+//                0,0,
+//                0,1,
+//                1,1,
+//                1,0,
+//                0,0,
+//                0,1,
+//                1,1,
+//                1,0,
+//                0,0,
+//                0,1,
+//                1,1,
+//                1,0,
+//                0,0,
+//                0,1,
+//                1,1,
+//                1,0
+//        };
+//
+//        int[] indices = {
+//                0,1,3,
+//                3,1,2,
+//                4,5,7,
+//                7,5,6,
+//                8,9,11,
+//                11,9,10,
+//                12,13,15,
+//                15,13,14,
+//                16,17,19,
+//                19,17,18,
+//                20,21,23,
+//                23,21,22
+//
+//        };
 
-                -0.5f,0.5f,0.5f,
-                -0.5f,-0.5f,0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                0.5f,0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                -0.5f,-0.5f,0.5f,
-                -0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,-0.5f,0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f
-        };
-
-        float[] textureCoords = {
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0
-        };
-
-        int[] indices = {
-                0,1,3,
-                3,1,2,
-                4,5,7,
-                7,5,6,
-                8,9,11,
-                11,9,10,
-                12,13,15,
-                15,13,14,
-                16,17,19,
-                19,17,18,
-                20,21,23,
-                23,21,22
-
-        };
-
-        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("space"));
+        RawModel model = OBJLoader.loadObjModel("Ship2", loader);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("ship"));
         TexturedModel texturedModel = new TexturedModel(model, texture);
 
         Entity entity = new Entity(texturedModel, new Vector3f(0,0,-5), 0,0,0,1);
@@ -104,7 +106,7 @@ public class MainGameLoop {
 
 
         while(!Display.isCloseRequested()){
-//            entity.increaseRotation(1,1,0);
+            entity.increaseRotation(0,0.5f,0);
             camera.move();
             renderer.prepare();
 
