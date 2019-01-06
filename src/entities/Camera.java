2a50fdcfd17cee3ba568e53876cc3868145dd094
Jiri Hauser <jirihauser94@gmail.com>
2019-01-06 21:30:25 +0100

    player with following camera

--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -1,30 +1,35 @@
 package entities;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
 
-    private final float CAMERA_SPEED = 0.08f;
+    private float distanceFromPlayer = 3;
+    private float angleAroundPlayer = 0;
 
-    private Vector3f position = new Vector3f(0,10,0);
-    private float pitch;
-    private float yaw = 180;
+    private Vector3f position = new Vector3f(0,0,0);
+    private float pitch = 20;
+    private float yaw = 0;
     private float roll;
 
+    private Player player;
+
+    public Camera(Player player) {
+        this.player = player;
+    }
+
     public void move() {
-        if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-            position.z -= CAMERA_SPEED;
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-            position.x += CAMERA_SPEED;
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-            position.x -= CAMERA_SPEED;
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-            position.z += CAMERA_SPEED;
-        }
+        calculateZoom();
+        calculatePitch();
+        calculateAngleAroundPlayer();
+
+        float horizontalDistance = calculateHorizontalDistance();
+        float verticalDistance = calculateVerticalDistance();
+
+        calculateCameraPosition(horizontalDistance, verticalDistance);
+        this.yaw = 180 - (player.getRotY() + angleAroundPlayer);
     }
 
 
@@ -43,5 +48,41 @@ public class Camera {
 
     public float getRoll() {
         return roll;
+    }
+
+    private void calculateCameraPosition(float horizontalDistance, float verticalDistance) {
+        float theta = player.getRotY() + angleAroundPlayer;
+
+        float offX = (float)(horizontalDistance*Math.sin(Math.toRadians(theta)));
+        float offZ = (float)(horizontalDistance*Math.cos(Math.toRadians(theta)));
+        position.x = player.getPosition().x - offX;
+        position.z = player.getPosition().z - offZ;
+        position.y = player.getPosition().y + verticalDistance;
+    }
+
+    private float calculateHorizontalDistance(){
+        return (float)(distanceFromPlayer *Math.cos(Math.toRadians(pitch)));
+    }
+    private float calculateVerticalDistance(){
+        return (float)(distanceFromPlayer *Math.sin(Math.toRadians(pitch)));
+    }
+
+    private void calculateZoom(){
+        float zoomLevel = Mouse.getDWheel() * 0.01f;
+        distanceFromPlayer -= zoomLevel;
+    }
+
+    private void calculatePitch(){
+        if (Mouse.isButtonDown(1)){
+            float pitchChange = Mouse.getDY() * 0.1f;
+            pitch -= pitchChange;
+        }
+    }
+
+    private void calculateAngleAroundPlayer(){
+        if (Mouse.isButtonDown(0)){
+            float angleChange = Mouse.getDX() * 0.3f;
+            angleAroundPlayer -= angleChange;
+        }
     }
 }
--- src/game/MainGameLoop.java
+++ src/game/MainGameLoop.java
@@ -3,6 +3,7 @@ package game;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
+import entities.Player;
 import models.RawModel;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
@@ -21,101 +22,109 @@ public class MainGameLoop {
 //        StaticShader shader = new StaticShader();
 //        EntityRenderer renderer = new EntityRenderer(shader);
 // cube
-// float[] vertices = {
-//                -0.5f,0.5f,-0.5f,
-//                -0.5f,-0.5f,-0.5f,
-//                0.5f,-0.5f,-0.5f,
-//                0.5f,0.5f,-0.5f,
-//
-//                -0.5f,0.5f,0.5f,
-//                -0.5f,-0.5f,0.5f,
-//                0.5f,-0.5f,0.5f,
-//                0.5f,0.5f,0.5f,
-//
-//                0.5f,0.5f,-0.5f,
-//                0.5f,-0.5f,-0.5f,
-//                0.5f,-0.5f,0.5f,
-//                0.5f,0.5f,0.5f,
-//
-//                -0.5f,0.5f,-0.5f,
-//                -0.5f,-0.5f,-0.5f,
-//                -0.5f,-0.5f,0.5f,
-//                -0.5f,0.5f,0.5f,
-//
-//                -0.5f,0.5f,0.5f,
-//                -0.5f,0.5f,-0.5f,
-//                0.5f,0.5f,-0.5f,
-//                0.5f,0.5f,0.5f,
-//
-//                -0.5f,-0.5f,0.5f,
-//                -0.5f,-0.5f,-0.5f,
-//                0.5f,-0.5f,-0.5f,
-//                0.5f,-0.5f,0.5f
-//        };
-//
-//        float[] textureCoords = {
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0
-//        };
-//
-//        int[] indices = {
-//                0,1,3,
-//                3,1,2,
-//                4,5,7,
-//                7,5,6,
-//                8,9,11,
-//                11,9,10,
-//                12,13,15,
-//                15,13,14,
-//                16,17,19,
-//                19,17,18,
-//                20,21,23,
-//                23,21,22
-//
-//        };
+ float[] vertices = {
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
 
-        RawModel model = OBJLoader.loadObjModel("Ship2", loader);
+                -0.5f,0.5f,0.5f,
+                -0.5f,-0.5f,0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                0.5f,0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                -0.5f,-0.5f,0.5f,
+                -0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,-0.5f,0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f
+        };
+
+        float[] textureCoords = {
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
+        };
+
+        int[] indices = {
+                0,1,3,
+                3,1,2,
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
+
+        };
+        RawModel cube = loader.loadToVAO(vertices, textureCoords, new float[0], indices);
+        TexturedModel texturedCube = new TexturedModel(cube, new ModelTexture(loader.loadTexture("ship")));
+        Entity cubeE = new Entity(texturedCube, new Vector3f(50,3,-5),0,0,0,1f);
+
+
+        RawModel model = OBJLoader.loadObjModel("ShipOBJ", loader);
         ModelTexture texture = new ModelTexture(loader.loadTexture("ship"));
         TexturedModel texturedModel = new TexturedModel(model, texture);
         texture.setShineDamper(10);
         texture.setReflectivity(1);
 
-        Entity entity = new Entity(texturedModel, new Vector3f(0,0,-5), 0,0,0,1);
-        Terrain terrain = new Terrain(0,0,loader, new ModelTexture(loader.loadTexture("podlaha")));
-        Terrain terrain2 = new Terrain(1,0,loader, new ModelTexture(loader.loadTexture("podlaha")));
-        Camera camera = new Camera();
+//        Entity entity = new Entity(texturedModel, new Vector3f(0,0,-5), 0,0,0,1);
+        Terrain terrain = new Terrain(0,-1,loader, new ModelTexture(loader.loadTexture("podlaha")));
+        Terrain terrain2 = new Terrain(-1,-1,loader, new ModelTexture(loader.loadTexture("podlaha")));
 
         Light light = new Light(new Vector3f(3000,2000,2000), new Vector3f(1,1,1));
 
+        Player player = new Player(texturedModel, new Vector3f(0,0,0),0,90,0,1f);
+        Camera camera = new Camera(player);
+
         MasterRenderer renderer = new MasterRenderer();
+
         while(!Display.isCloseRequested()){
-            entity.increaseRotation(0f,.5f,0f);
             camera.move();
+            player.move();
+            renderer.processEntity(player);
             renderer.processTerrain(terrain);
             renderer.processTerrain(terrain2);
-            renderer.processEntity(entity);
+//            renderer.processEntity(entity);
             renderer.render(light, camera);
             DisplayManager.updateDisplay();
         }
